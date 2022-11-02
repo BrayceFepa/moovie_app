d6b655d76fe1bae12db51953e765b9fa2cb49153
Brayce <braycefepa@gmail.com>
2022-11-02 20:42:29 +0100

    En fin terminé !

---
+++ src/Components/MoovieGrid/MoovieGrid.jsx
@@ -0,0 +1,133 @@
+import React, { useCallback } from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import tmbApi, { category, movieType, tvType } from '../../api/tmdbApi';
+import Button, { OutlineButton } from '../Button/Button';
+import MovieCard from '../MovieCard/MovieCard';
+import Input from '../Input/Input';
+
+import './MoovieGrid.scss';
+
+const MoovieGrid = props => {
+
+    const [items, setItems] = useState([]);
+
+    const [page, setPage] = useState(1);
+
+    const [totalPage, setTotalPage] = useState(0);
+
+    const { keyword } = useParams();
+
+    useEffect(() => {
+        const getList = async () => {
+            let response = null;
+
+            if (keyword === undefined) {
+                const params = {};
+
+                switch (props.category) {
+                    case category.movie:
+                        response = await tmbApi.getMovieList(movieType.upcoming, { params });
+                        break;
+                    default:
+                        response = await tmbApi.getTvList(tvType.popular, { params });
+                }
+            } else {
+                const params = { query: keyword };
+                response = await tmbApi.search(props.category, { params });
+            }
+
+            setItems(response.results);
+            setTotalPage(response.total_pages);
+
+        }
+        getList();
+    }, [props.category, keyword]);
+
+    const loadMore = async () => {
+         let response = null;
+
+            if (keyword === undefined) {
+                const params = {page: page + 1};
+
+                switch (props.category) {
+                    case category.movie:
+                        response = await tmbApi.getMovieList(movieType.upcoming, { params });
+                        break;
+                    default:
+                        response = await tmbApi.getTvList(tvType.popular, { params });
+                }
+            } else {
+                const params = { page: page + 1, query: keyword };
+                response = await tmbApi.search(props.category, { params });
+            }
+
+            setItems([...items, ...response.results]);
+            setPage(page + 1);
+    }
+
+  return (
+      <>
+          <div className="section mb-3">
+              <MovieSearch category={props.category} keyword={keyword} />
+          </div>
+          
+        <div className='movie-grid' >
+          {items.map((item, i)=> <MovieCard category={props.category} item={item} key={i} /> )}
+          </div>
+          {
+              page < totalPage ? (
+                  <div className="movie-grid__loadmore">
+                      <OutlineButton className="small" onClick={loadMore} >Load More</OutlineButton>
+                  </div>
+              ): null
+          }
+      </>
+  )
+}
+
+const MovieSearch = props => {
+
+    const navigate = useNavigate();
+
+    const [keyword, setKeyword] = useState(props.keyword ? props.keyword : '');
+
+    const goToSearch = useCallback(
+        () => {
+            if (keyword.trim().length > 0) {
+                navigate(`/${category[props.category]}/search/${keyword}`);
+            }
+        },
+        [keyword, props.category, navigate]
+    );
+
+    useEffect(() => {
+        const enterEvent = (e) => {
+            e.preventDefault();
+            if (e.keyCode === 13) {
+                goToSearch();
+            }
+        }
+
+        document.addEventListener('keyup', enterEvent);
+
+        return () => {
+            document.removeEventListener('keyup', enterEvent);
+        };
+    }, [keyword, goToSearch])
+
+    return (
+        <div className="movie-search">
+            <Input
+                type='text'
+                placeholder='Enter keyword'
+                value={keyword}
+                onChange={(e)=> setKeyword(e.target.value)}
+            />
+            <Button className='small' onClick={goToSearch} >Search</Button>
+        </div>
+    )
+}
+
+export default MoovieGrid;
--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import PageHeader from '../Components/page-header/PageHeader';
+
+import { category as cate } from '../api/tmdbApi';
+import MoovieGrid from '../Components/MoovieGrid/MoovieGrid';
 
 const Catalog = () => {
+
+  const { category } = useParams();
+  
+
   return (
-    <div>Catalog</div>
+    <>
+      <PageHeader>
+        {category === cate.movie ? 'Movies': 'TV Series'}
+      </PageHeader>
+      <div className="container">
+        <div className="section mb-3">
+          <MoovieGrid category={category}  />
+        </div>
+      </div>
+    </>
   )
 }
 
