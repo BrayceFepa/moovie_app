aa042ea6efe70f4619392352bf42837f0ab36881
brayce <braycefepa@gmail.com>
2025-04-04 11:35:02 +0100

    customize

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -49,7 +49,7 @@ const Header = () => {
       <div className="header__wrap container">
         <div className="logo">
           <img src={logo} alt="" />
-          <Link to="/" >tMovies</Link>
+          <Link to="/" >Brayce Movies</Link>
         </div>
 
         <ul className="header__nav">
